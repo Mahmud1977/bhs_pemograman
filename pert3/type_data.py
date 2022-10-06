c3c6561db2724b089cc1574d71bb87200f7e0cf9
Mahmud1977 <mahmud1977.laopo@gamil.com>
2022-10-06 23:50:16 +0700

    Test Commit

---
+++ pert3/type_data.py
@@ -0,0 +1,48 @@
+# type data python
+
+# type data integer number
+a = 12
+print(a)
+print(type(a))
+
+# type data float
+b = 12.5
+print(b)
+print(type(b))
+
+# type data complex
+c = 12j
+print(c)
+print(type(c))
+
+# type data string
+A = "Selamat Datang"
+print(A)
+print(type(A))
+
+# type data tuple
+B = ("A","B","C")
+C = 12,5,3,1
+print(B,C)
+print(type(B),type(C))
+
+# type data lis
+D = ["A","B","C"]
+print(D)
+print(type(D))
+
+#type data set
+D ={"saya","sedang","kuliah"}
+print(D)
+print(type(D))
+
+# type data dictonary
+D={"nama: Mahmud S Laopo, umur: 46"}
+print(D)
+print(type(D))
+
+# type data bool
+a = True
+b = False
+print(a,b)
+print(type(a),type(b))
